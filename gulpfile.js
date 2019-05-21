9489601a07db5a29cf6a140a0a34a0dc8da8048d
PaddiM8 <paddim8@pm.me>
2019-05-21 14:37:34 +0200

    js minimize

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,31 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var jsdoc = require("gulp-jsdoc3");
+const gulp   = require("gulp");
+const sass   = require("gulp-sass");
+const jsdoc  = require("gulp-jsdoc3");
+const terser = require("gulp-terser");
 
 sass.compiler = require("node-sass");
 
 gulp.task("sass", function() {
    return gulp.src("./src/wwwroot/scss/*.scss")
-      .pipe(sass().on("error", sass.logError))
+      .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
       .pipe(gulp.dest("./src/wwwroot/css"));
 });
 
+gulp.task("js", function() {
+   return gulp.src("./src/wwwroot/js/*.js")
+      .pipe(terser())
+      .pipe(gulp.dest("./src/wwwroot/js"));
+});
+
 gulp.task("doc", function(cb) {
-   gulp.src(["README.md", "./wwwroot/js/*.js"], {read: false})
-       .pipe(jsdoc(cb));
+   return gulp.src(["README.md", "./wwwroot/js/*.js"], {read: false})
+       .pipe(jsdoc(cb))
+       .pipe(gulp.dest("./docs"));
 });
 
-gulp.task("sass:watch", function() {
-   gulp.watch("./src/wwwroot/scss/*.scss", ["sass"]);
+gulp.task("watch", function() {
+   gulp.watch("./src/wwwroot/scss/*.scss", gulp.series("sass"));
+   gulp.watch("./src/wwwroot/js/*.js", gulp.series("js"));
 });
 
-gulp.task("default", ["sass, watch"]);
+gulp.task("default", gulp.series("sass", "js", "doc", "watch"));

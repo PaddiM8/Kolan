630b758c85b85150eab3192f92f77fd3d56972ba
PaddiM8 <=>
2019-05-29 14:50:59 +0200

    dialogs work in progress

--- src/wwwroot/ts/boards.ts
+++ src/wwwroot/ts/boards.ts
@@ -6,20 +6,26 @@ import { addBoardDialog } from "./dialogs/addBoardDialog";
 window.addEventListener("load", () => new Boards().initiate());
 
 class Boards {
+   private addDialog: any;
+
    initiate() {
+      // Events
       document.getElementById("addBoard").addEventListener("click",
          this.onAddBoardClick);
+
+      // Prepare dialog
+      this.addDialog = document.createElement("dialog-box");
+      dialog.id = "addBoardDialog";
+      dialog.inputs = addBoardDialog;
+      document.body.appendChild(dialog);
    }
 
    onAddBoardClick() {
-      // Show dialog box
-      const dialog: any = document.createElement("dialog-box");
-      dialog.inputs = addBoardDialog;
-      document.body.appendChild(dialog);
+      this.addDialog.shown = true;
 
-      // Add board item to list
+      /* Add board item to list
       const boardListController = new BoardListController(document
          .querySelector(".board-list tasklist"));
-      boardListController.addBoard("Name", "Description");
+      boardListController.addBoard("Name", "Description");*/
    };
 }
--- src/wwwroot/ts/components/dialogBox.ts
+++ src/wwwroot/ts/components/dialogBox.ts
@@ -6,9 +6,11 @@ import { LitElement, html, css, property, customElement } from 'lit-element';
 @customElement('dialog-box')
 export class DialogBox extends LitElement {
    @property() inputs;
+   @property({type: boolean}) shown;
 
    render() {
-      return html``;
+      return html`${inputs.map(x => 
+         html`${x.value}<br /><input type="${x.inputType}" placeholder="${x.value}" /><br />`)}`;
    }
 
    update() {

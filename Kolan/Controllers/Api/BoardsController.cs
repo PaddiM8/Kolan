5f8bc8755ad72fc992e7ca42fa6e9d981cfbb144
PaddiM8 <paddim8@pm.me>
2019-12-29 17:58:10 +0100

    api documentation

--- Kolan/Controllers/Api/BoardsController.cs
+++ Kolan/Controllers/Api/BoardsController.cs
@@ -62,7 +62,7 @@ namespace Kolan.Controllers.Api
         /// Create a root board for a user.
         /// </summary>
         /// <param name="board">Board object</param>
-        /// <returns>The id assigned  to the new board</returns>
+        /// <returns>The id assigned to the new board</returns>
         [HttpPost]
         public async Task<IActionResult> Create([FromForm]Board board)
         {
@@ -71,9 +71,15 @@ namespace Kolan.Controllers.Api
             return Ok(new { id = id });
         }
 
+        /// <summary>
+        /// Create a child board inside another board.
+        /// </summary>
+        /// <param name="parentId">Parent board id</param>
+        /// <param name="groupId">Id of group to create it under</param>
+        /// <param name="board">Board object</param>
+        /// <returns>The id assigned to the new board</returns>
         [HttpPost("{parentId}")]
-        public async Task<IActionResult> Create(string parentId, [FromForm]string groupId, [FromForm]Board
-                                                board)
+        public async Task<IActionResult> Create(string parentId, [FromForm]string groupId, [FromForm]Board board)
         {
             string id = await _uow.Boards.AddAsync(board, groupId,
                                                    User.Identity.Name); // Add board to parent board
@@ -82,24 +88,53 @@ namespace Kolan.Controllers.Api
             return Ok(new { id = id });
         }
 
+        /// <summary>
+        /// Edit a child board.
+        /// </summary>
+        /// <param name="parentId">Parent board id</param>
+        /// <param name="newBoardContent">Board object with the new values, containing the board id</param>
         [HttpPut("{parentId}")]
         public async Task<IActionResult> Edit(string parentId, [FromForm]Board newBoardContent)
         {
             await _uow.Boards.EditAsync(newBoardContent);
             await _boardHubContext.Clients.Group(parentId).EditBoard(newBoardContent);
 
-            return new EmptyResult();
+            return Ok();
         }
 
+        /// <summary>
+        /// Delete a child board
+        /// </summary>
+        /// <param name="parentId">Parent board id</param>
+        /// <param name="boardId">Id of the board to delete</param>
         [HttpDelete("{parentId}")]
         public async Task<IActionResult> Delete(string parentId, [FromForm]string boardId)
         {
             await _uow.Boards.DeleteAsync(boardId);
             await _boardHubContext.Clients.Group(parentId).DeleteBoard(boardId);
 
-            return new EmptyResult();
+            return Ok();
         }
 
+        /// <summary>
+        /// Move a root board.
+        /// </summary>
+        /// <param name="boardId">Id of the board to move</param>
+        /// <param name="targetId">Id of the board it will be placed under</param>
+        [HttpPost("ChangeOrder")]
+        public async Task<IActionResult> ChangeOrder([FromForm]string boardId, [FromForm]string targetId)
+        {
+            await _uow.Boards.MoveAsync(User.Identity.Name, boardId, targetId, true);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Move a child board.
+        /// </summary>
+        /// <param name="parentId">Parent board id</param>
+        /// <param name="boardId">Id of the board to move</param>
+        /// <param name="targetId">Id of the board it will be placed under</param>
         [HttpPost("{parentId}/ChangeOrder")]
         public async Task<IActionResult> ChangeOrder(string parentId, [FromForm]string
                 boardId, [FromForm]string targetId)
@@ -107,37 +142,44 @@ namespace Kolan.Controllers.Api
             await _uow.Boards.MoveAsync(parentId, boardId, targetId, false);
             await _boardHubContext.Clients.Group(parentId).MoveBoard(boardId, targetId); // Send to client
 
-            return new EmptyResult();
+            return Ok();
         }
 
-        [HttpPost("ChangeOrder")]
-        public async Task<IActionResult> ChangeOrder([FromForm]string boardId, [FromForm]string targetId)
-        {
-            await _uow.Boards.MoveAsync(User.Identity.Name, boardId, targetId, true);
-
-            return new EmptyResult();
-        }
-
+        /// <summary>
+        /// Get a list of a boards collaborators.
+        /// </summary>
+        /// <param name="id">Id of the board</param>
+        /// <returns>A list of usernames (as strings)</returns>
         [HttpGet("{id}/Users")]
         public async Task<object> GetUsers(string id)
         {
             return await _uow.Boards.GetUsersAsync(id);
         }
 
+        /// <summary>
+        /// Add a user to a board as a collaborator.
+        /// </summary>
+        /// <param name="id">Id of the board</param>
+        /// <param name="username">Username of the user to add</param>
         [HttpPost("{id}/Users")]
         public async Task<IActionResult> AddUser(string id, [FromForm]string username)
         {
             await _uow.Boards.AddUserAsync(id, username);
 
-            return new EmptyResult();
+            return Ok();
         }
 
+        /// <summary>
+        /// Remove a collaborator from a board.
+        /// </summary>
+        /// <param name="id">Id of the board</param>
+        /// <param name="username">Username of the user to remove</param>
         [HttpDelete("{id}/Users")]
         public async Task<IActionResult> RemoveUser(string id, [FromForm]string username)
         {
             await _uow.Boards.RemoveUserAsync(id, username);
 
-            return new EmptyResult();
+            return Ok();
         }
     }
 }

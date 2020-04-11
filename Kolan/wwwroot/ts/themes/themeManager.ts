1cf362fcc4c701ef40a34e6e3cb8d80637276804
PaddiM8 <paddim8@pm.me>
2020-04-11 19:19:36 +0200

    automatically set default theme, using prefers-color-scheme

--- Kolan/wwwroot/ts/themes/themeManager.ts
+++ Kolan/wwwroot/ts/themes/themeManager.ts
@@ -1,5 +1,16 @@
 export class ThemeManager {
     public static injectStyle(): void {
+        if (!this.getTheme()) {
+            document.body.insertAdjacentHTML("beforeend", "<div id='themeTester' aria-hidden></div>");
+            const style = window.getComputedStyle(document.getElementById("themeTester"))
+
+            if (style.getPropertyValue("background-color") == "rgba(0, 0, 0, 0)") {
+                this.setTheme("dark");
+            } else {
+                this.setTheme("light");
+            }
+        }
+
         document.head.insertAdjacentHTML("beforeend", this.getStyle());
     }
 

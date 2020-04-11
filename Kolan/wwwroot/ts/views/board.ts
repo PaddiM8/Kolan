62148fa34b6078a1137d05c8456207f3748577ac
PaddiM8 <paddim8@pm.me>
2020-04-11 18:51:47 +0200

    board error codes

--- Kolan/wwwroot/ts/views/board.ts
+++ Kolan/wwwroot/ts/views/board.ts
@@ -300,6 +300,8 @@ export class Board extends View {
             ToastController.new("Loaded board", ToastType.Info);
         }).catch((req) => {
             if (req.status == 404) this.setTitle("404 - Board does not exist", []);
+            else if (req.status > 400 && req.status < 499) this.setTitle(`${req.status} - Client error`, []);
+            else if (req.status > 500 && req.status < 599) this.setTitle(`${req.status} - Server error`, []);
             console.log(req);
         });
     }
